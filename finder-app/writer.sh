6eaab40b6b41dd24d2bd313e15b9bace91582e9d
Me <me@Ubuntu.myguest.virtualbox.org>
2024-10-13 01:07:36 -0500

    Added return code Check on file and directory creation.

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -11,6 +11,19 @@ fi
 FILEWITHDIR=$1
 mkdir -p "${FILEWITHDIR%/*}" && touch "$FILEWITHDIR"
 
+#Check return code
+if [ $? -ne 0 ]
+then
+	echo "Could not create file and directory"
+	exit 1
+fi
+
 #Write the contents that was passed into script
 echo $2 > "$FILEWITHDIR"
 
+#Check return code
+if [ $? -ne 0 ]
+then
+	echo "Could not write file"
+	exit 1
+fi
